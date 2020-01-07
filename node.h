45562a4c1722d2373d882b8f44dd812a64b1c960
dbhan08 <38201130+dbhan08@users.noreply.github.com>
2020-01-07 09:29:23 -0800

    Add files via upload

---
+++ node.h
@@ -0,0 +1,26 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <iostream>
+#include <cstring>
+#include "Student.h"
+
+using namespace std;
+
+class Node{
+public:
+    Node();
+    ~Node();
+    void setStudent(Student* newStudent);
+    Student* getStudent();
+    void setNext(Node* newNode);
+    Node* getNext();
+private:
+    Student* student;
+    Node* nextNode;
+    
+    
+    
+    
+};
+#endif
